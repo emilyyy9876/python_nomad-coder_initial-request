a921c603d1489c5108b1781d32cd456578b42b70
emilyyy9876 <106860186+emilyyy9876@users.noreply.github.com>
2023-10-14 03:57:29 +0000

    #5.7 Job Extraction (11:14)
    - list 안의 span 들을 가져옴
    - 하지만 span의 class 명이 똑같은게 있어서, 다른 변수로 지정하고 싶음-> list안 item의 갯수를 알면 한줄로, 각각의 item마다 변수를 지정할 수 있는 방법?
    - class명 겹치는 span말고, 다른 span 가져오기 -> .find 메소드 사용함

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ for job_section in jobs:
   job_posts = job_section.find_all("li")
   job_posts.pop(-1)
   for post in job_posts:
-    print(post)
-    print("///////////////////")
-# li .feature > div .tooltip > 
+    anchors = post.find_all('a')
+    anchor = anchors[1]
+    link = anchor['href']
+    company, kind, region = anchor.find_all("span",class_="company")
+    print(company, kind, region)
+    title = anchor.find("span", class_="title")
+    print(company, kind, region, title)
+    print("//////////////////")
+    print("//////////////////")
+
+"""
+list_of_numbers = [1,2,3]
+first, second, third = list_of_numbers
+print(first, second, third)
+"""
