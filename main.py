c6ca62ad7f7c4017476c92bd9261b55dc5cef898
emilyyy9876 <106860186+emilyyy9876@users.noreply.github.com>
2023-10-14 04:47:34 +0000

    #5.9 Recap (10:44)
    - 현재 'job_Data'안의 'link'값이 상대경로.....절대경로로바꿔줌

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ for job_section in jobs:
     company, kind, region = anchor.find_all("span",class_="company")
     title = anchor.find("span", class_="title")
     job_data = {
+      'link': f"https://weworkremotely.com/{link}",
       'company': company.string,
       'region': region.string,
       'title': title.string
