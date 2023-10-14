a538b0481b942d7ec5408eedbe8a320f62e93337
emilyyy9876 <106860186+emilyyy9876@users.noreply.github.com>
2023-10-14 04:18:54 +0000

    #5.8 Saving Results (06:16)
    - 가져온 span들의 tag 들을 없애고 string만 남기고 싶음 -> .string 메소드 사용
    - span들을 dict로 만들고, 만든 dict를 조건문 밖으로 끄집어내고 싶음->바깥에 빈 array만들고, .append method사용

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup
 base_url = "https://weworkremotely.com/remote-jobs/search?&term="
-search_term = "python"
+search_term = "next"
 
 response = get(f"{base_url}{search_term}")
 if response.status_code !=200:
   print("Can't request website!")
 else:
+  results = []
   soup = BeautifulSoup(response.text,'html.parser')
   jobs = soup.find_all('section', class_="jobs")
 for job_section in jobs:
@@ -17,12 +18,16 @@ for job_section in jobs:
     anchor = anchors[1]
     link = anchor['href']
     company, kind, region = anchor.find_all("span",class_="company")
-    print(company, kind, region)
     title = anchor.find("span", class_="title")
-    print(company, kind, region, title)
-    print("//////////////////")
-    print("//////////////////")
-
+    job_data = {
+      'company': company.string,
+      'region': region.string,
+      'title': title.string
+    }
+    results.append(job_data)
+for result in results:
+  print(result)
+  print("//////////////////")
 """
 list_of_numbers = [1,2,3]
 first, second, third = list_of_numbers
